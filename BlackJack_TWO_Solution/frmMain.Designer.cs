74b86e403fde250f32c1189f219e1136aef2f25f
Tchollo88 <Tcholloman@gmail.com>
2024-02-28 09:12:33 -0800

    Created a developement branch as failsafe for code, make branches off this branch

--- BlackJack_TWO_Solution/frmMain.Designer.cs
+++ BlackJack_TWO_Solution/frmMain.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmMain";
+            this.Text = "TWO : BlackJack Mania";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
